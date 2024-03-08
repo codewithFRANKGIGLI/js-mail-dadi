255cdb2145b9ca382c875e743a1dbc95eed57731
codewithFRANKGIGLI <gigli.francesco099@gmail.com>
2024-03-08 15:49:14 +0100

    feat: diceroll if

--- Gioco dei dadi/script.js
+++ Gioco dei dadi/script.js
@@ -1,30 +1,24 @@
 // Esercizio di oggi:
 // nome repo: js-mail-dadi
 // Gioco dei dadi
-
+// Creo l'array vuoto:
 const faceDice = [];
 // Popolo l'array con i numeri da 1 a 6:
 for (let i = 0; i < 6; i++) {
   faceDice.push(i + 1);
 }
+// Creo le variabili che 'pescano' un elemento casuale dall'array:
 let userRoll = faceDice[Math.floor(Math.random() * faceDice.length)];
 console.log(userRoll);
 let aiRoll = faceDice[Math.floor(Math.random() * faceDice.length)];
 console.log(aiRoll);
 
-
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
-// Mail
-// Chiedi all’utente la sua email,
-// controlla che sia nella lista di chi può accedere,
-// stampa un messaggio appropriato sull’esito del controllo.
-// Per la soluzione di questo esercizio NON è consentito utilizzare .includes di js!
-// Prima di partire a scrivere codice poniamoci qualche domanda:
-// Che ci sia un array da qualche parte?
-// Se dobbiamo confrontare qualcosa che "cosa" ci serve?
-// Consigli del giorno:
-// 1. scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
-// 2. javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
-// 3. si ma noi cosa vogliamo fare?
-// 4. torniamo a scrivere in italiano
-// 5. proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano
+const winner;
+if(userRoll == aiRoll) {
+  prompt('Parità');
+} else if(userRoll < aiRoll) {
+  prompt('Hai perso');
+} else {
+  prompt('Hai Vinto!');
+}
