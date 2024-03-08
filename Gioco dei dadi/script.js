f5cf96b7fbb6ced301e8978ffaf90727de433f11
codewithFRANKGIGLI <gigli.francesco099@gmail.com>
2024-03-08 15:33:36 +0100

    feat: diceroll

--- Gioco dei dadi/script.js
+++ Gioco dei dadi/script.js
@@ -1,7 +1,18 @@
 // Esercizio di oggi:
 // nome repo: js-mail-dadi
 // Gioco dei dadi
-// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+
+const faceDice = [];
+// Popolo l'array con i numeri da 1 a 6:
+for (let i = 0; i < 6; i++) {
+  faceDice.push(i + 1);
+}
+let userRoll = faceDice[Math.floor(Math.random() * faceDice.length)];
+console.log(userRoll);
+let aiRoll = faceDice[Math.floor(Math.random() * faceDice.length)];
+console.log(aiRoll);
+
+
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 // Mail
 // Chiedi all’utente la sua email,
@@ -16,4 +27,4 @@
 // 2. javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
 // 3. si ma noi cosa vogliamo fare?
 // 4. torniamo a scrivere in italiano
-// 5. proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
+// 5. proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano
